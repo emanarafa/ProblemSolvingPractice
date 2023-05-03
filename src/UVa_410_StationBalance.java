24357bb1d9063a0a9384d194b2653aa22dca31c7
eman arafa <eman.abourakha@gmail.com>
2023-05-03 15:14:11 +0200

    base file

--- src/UVa_410_StationBalance.java
+++ src/UVa_410_StationBalance.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class UVa_410_StationBalance {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
+public class UVa_410_StationBalance {
+    public static void main(String[] args) throws IOException {
+        //BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new FileReader("input.txt"));
+
+    }
 }
