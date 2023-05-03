4da545ec12e447d1e1460f4c2a1c21247c505fbf
eman arafa <eman.abourakha@gmail.com>
2023-05-03 16:33:19 +0200

    base file

---
+++ src/UVa_1193_RadarInstallation.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class UVa_1193_RadarInstallation {
+    public static void main(String[] args) throws IOException {
+        Scanner sc= new Scanner(new FileReader("input.txt"));
+        PrintWriter out = new PrintWriter(System.out);
+
+
+    }
+    static class Scanner{
+        StringTokenizer st;
+        BufferedReader br;
+
+        public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+
+        public Scanner(FileReader r){	br = new BufferedReader(r);}
+
+        public String next() throws IOException
+        {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+
+        }
+
+        public long nextLong() throws IOException {return Long.parseLong(next());}
+
+        public String nextLine() throws IOException {return br.readLine();}
+
+        public double nextDouble() throws IOException { return Double.parseDouble(next()); }
+
+        public boolean ready() throws IOException {return br.ready();}
+    }
+}
