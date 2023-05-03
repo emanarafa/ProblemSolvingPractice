ede69a6649793f618db36f78867c3a0473d30bf4
eman arafa <eman.abourakha@gmail.com>
2023-05-03 15:24:34 +0200

    added print writer

--- src/UVa_410_StationBalance.java
+++ src/UVa_410_StationBalance.java
@@ -1,13 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.Buffer;
 
 public class UVa_410_StationBalance {
     public static void main(String[] args) throws IOException {
         //BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         BufferedReader br=new BufferedReader(new FileReader("input.txt"));
+        PrintWriter out=new PrintWriter(System.out);
 
     }
 }
