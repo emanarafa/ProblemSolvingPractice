239844e97f233c3556aeb57967026996a0dfb7d2
eman arafa <eman.abourakha@gmail.com>
2023-05-04 20:38:23 +0200

    adding some changes to minimal

--- src/UVa_10020_MinimalCoverage.java
+++ src/UVa_10020_MinimalCoverage.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class UVa_10020_MinimalCoverage {
@@ -8,8 +9,42 @@ public class UVa_10020_MinimalCoverage {
           Scanner sc=new Scanner(new FileReader("input.txt"));
           PrintWriter out=new PrintWriter(System.out);
           int testCases= sc.nextInt();
+          while(testCases-->0){
+            sc.nextLine();
+            int m= sc.nextInt();
+            int left; int right;
+              PriorityQueue<Pair>priorityQueue=new PriorityQueue<>();
+            while (true){
+                left= sc.nextInt();right= sc.nextInt();
+                if (left==0 && right==0)break;
+                priorityQueue.offer(new Pair(left,right));
+            }
+            myComparator myComparator = new myComparator();
+            PriorityQueue<Pair>result=new PriorityQueue<>(myComparator);
+            while (!priorityQueue.isEmpty()){
+                Pair curr=priorityQueue.poll();
+                if (curr.start<=0 && curr.finish>=m){
+                   result.clear();
+                    result.add(curr);break;
+                }else{
+                 if(priorityQueue.peek().start<=curr.finish && priorityQueue.peek().start>=curr.start ){
+
+                 }
+
+                }
+
+            }
+          }
 
 
+    }
+    static class myComparator implements Comparator<Pair>{
+
+        @Override
+        public int compare(Pair o1, Pair o2) {
+            if (o1.start!=o2.start)return Integer.compare(o1.start, o2.start);
+            return Integer.compare(o1.finish, o2.finish);
+        }
     }
     static class Pair implements Comparable<Pair> {
         int start;
